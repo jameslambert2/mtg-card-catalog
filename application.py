fe59d92f40d57a90ea7c42b6a2d5af5d3f21b787
James Lambert <james.lambert3995@gmail.com>
2025-08-07 15:58:50 -0500

    Adding bg change comments (doesn't look good as blue, but there for when I change it)

--- application.py
+++ application.py
@@ -37,41 +37,62 @@ def on_button_click(title, type, subtype, rarity, color, set_shortened):
 
 def main():
     root = Tk()
+    # root.config(bg="BLUE")
     root.title("MTG Card Catalog")
     root.geometry("250x250")  # width x height
     my_label = Label(root, text="Welcome to the MTG Card Catalog!")
-    my_label.grid(row=0, column=0, columnspan=2)  # Places the label in the window
-    Label(root, text = "Title").grid(row=1, column=0)
+    # my_label.config(bg="BLUE")
+    my_label.grid(row=0, column=0, columnspan=3)  # Places the label in the window
+    tmp=Label(root, text = "Title")
+    # tmp.config(bg="BLUE")
+    tmp.grid(row=1, column=0)
     title_entry_box = Entry(root)
+    # title_entry_box.config(bg="BLUE")
     title_entry_box.grid(row=1, column=1)
     
-    Label(root, text = "Type").grid(row=2, column=0)
+    tmp = Label(root, text = "Type")
+    # tmp.config(bg="BLUE")
+    tmp.grid(row=2, column=0)
     type_entry_box = Entry(root)
+    # type_entry_box.config(bg="BLUE")
     type_entry_box.grid(row=2, column=1)
     
-    Label(root, text = "Subtype").grid(row=3, column=0)
+    tmp =Label(root, text = "Subtype")
+    # tmp.config(bg="BLUE")
+    tmp.grid(row=3, column=0)
     subtype_entry_box = Entry(root)
+    # subtype_entry_box.config(bg="BLUE")
     subtype_entry_box.grid(row=3, column=1)
 
-    Label(root, text = "Rarity").grid(row=4, column=0)
+    tmp = Label(root, text = "Rarity")
+    # tmp.config(bg="BLUE")
+    tmp.grid(row=4, column=0)
     active_var = StringVar(root)
-    rarity_drop_box = OptionMenu(root, active_var, Rarity.COMMON.name, Rarity.UNCOMMON.name, Rarity.RARE.name, Rarity.MYTHIC_RARE.name)
-    rarity_drop_box.config(width=15)
+    rarity_drop_box = OptionMenu(root, active_var, "", Rarity.COMMON.name, Rarity.UNCOMMON.name, Rarity.RARE.name, Rarity.MYTHIC_RARE.name)
+    rarity_drop_box.config(width=15) #, bg="BLUE")
     rarity_drop_box.grid(row=4, column=1)
 
-    Label(root, text = "Color").grid(row=5, column=0)
+    tmp = Label(root, text = "Color")
+    # tmp.config(bg="BLUE")
+    tmp.grid(row=5, column=0)
     active_var2 = StringVar(root)
-    color_drop_box = OptionMenu(root,active_var2, *(Color.keys()))
-    color_drop_box.config(width=15)
+    tmp = list(Color.keys())
+    tmp.remove("VARIABLE")
+    color_drop_box = OptionMenu(root,active_var2, "", *(tmp))
+    color_drop_box.config(width=15) #, bg="BLUE")
     color_drop_box.grid(row=5, column=1)
 
-    Label(root, text = "Set").grid(row=6, column=0)
+    tmp = Label(root, text = "Set")
+    # tmp.config(bg="BLUE")
+    tmp.grid(row=6, column=0)
     set_entry_box = Entry(root)
+    # set_entry_box.config(bg="BLUE")
     set_entry_box.grid(row=6, column=1)
 
     my_button = Button(
         root, text="Search", command=lambda: on_button_click(title_entry_box.get(), type_entry_box.get(), subtype_entry_box.get(), active_var.get(), active_var2.get(), set_entry_box.get())
     )
+    # my_button.config(bg="BLUE")
     my_button.grid(row=7, column=1)  # Places the button in the window
     root.mainloop()
 
